1ecadc11d75ebf0c1b83137efda83ecc6b4dc48a
Marcelo Amorim <marceloamorim@MBP-de-Marcelo.lan>
2023-07-06 00:07:19 -0300

    added backend

---
+++ backend/oberon/src/main/java/com/br/oberon/OberonApplication.java
@@ -0,0 +1,13 @@
+package com.br.oberon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OberonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OberonApplication.class, args);
+	}
+
+}
---
+++ backend/oberon/src/test/java/com/br/oberon/OberonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.oberon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OberonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
